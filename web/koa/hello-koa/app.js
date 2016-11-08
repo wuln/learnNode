995e08f4d78cb1d42496151628a870dea1bfa00d
wulina <18729355828@163.com>
2016-11-08 16:05:03 +0800

    fixed hello-koa

--- web/koa/hello-koa/app.js
+++ web/koa/hello-koa/app.js
@@ -5,14 +5,20 @@ const app = new Koa();
 
 // 参数ctx是由koa传入的封装了request和response的变量，next是koa传入的将要处理的下一个异步函数。
 app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} : ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware            
+});
+
+app.use(async (ctx, next) => {
     const start = new Date().getTime();
     await next();
     const ms = new Date().getTime() - start;
-    console.log(`${ctx.request.method} ${ctx.request.url}: ${ms}ms`);
+    console.log(`Time: ${ms}ms`);
     ctx.response.set('X-Response-Time', `${ms}ms`);
 });
-// 对于任何请求，app将调用该异步函数处理请求：
+// app调用该异步函数处理请求：
 app.use(async (ctx, next) => {
+     // 处理下一个异步函数
     await next();
      // 设置response的Content-Type:
     ctx.response.type = 'text/html';
--- web/koa/hello-koa/start.js
+++ web/koa/hello-koa/start.js
@@ -1,3 +1,8 @@
+// 启动app.js前自动让Babel转码
+// 用Babel转码时，需要指定presets和plugins。
+// 第一个require()是Node正常加载babel-core/register的过程
+// 然后，Babel会用自己的require()替换掉Node的require(),所有代码均会被Babel自动转码后再加载
+
 require('babel-core/register')({
     presets: ['stage-3']
 });
