780c87d21a6abd96fc6d1b1ee77edb6446cb3e68
wulina <18729355828@163.com>
2016-11-14 11:13:04 +0800

    add vue-api

---
+++ web/vue/vue-api/app.js
@@ -0,0 +1,25 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const controller = require('./controller');
+const rest = require('./rest');
+const app = new Koa();
+const isProduction = process.env.NODE_ENV === 'production';
+
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);   
+    await next();   
+});
+
+let staticFiles = require('./static-files');
+app.use(staticFiles('/static/', __dirname + '/static'));
+
+//解析request body
+app.use(bodyParser());
+
+// 给ctx绑定rest()
+app.use(rest.restify());
+
+app.use(controller());
+
+app.listen(3000);
+console.log('app started at port 3000...');
